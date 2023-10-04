d5a4f493b38ab30c2fb2a0f3955d68a220eec3e5
Connell Hagen <hage0686@umn.edu>
2023-10-04 08:59:39 -0500

    Add Pace Converter Modes

---
+++ Converters/ConversionModeToBoolConverter.cs
@@ -0,0 +1,40 @@
+﻿using PaceCalculator.MVVM.ViewModel;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PaceCalculator.Converters
+{
+    class ConversionModeToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && value.GetType().IsEnum)
+            {
+                return (Enum.Equals(value, parameter));
+            }
+            else
+            {
+                return DependencyProperty.UnsetValue;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool? v = value as bool?;
+            if (v == null) { throw new ArgumentNullException(); }
+
+            if ((bool)v) { return parameter;  }
+
+            if ((PaceConversionMode)parameter == PaceConversionMode.MI_TO_KM)
+            {
+                return PaceConversionMode.KM_TO_MI;
+            }
+            else
+            {
+                return PaceConversionMode.MI_TO_KM;
+            }
+        }
+    }
+}
--- MVVM/ViewModel/PaceConversionViewModel.cs
+++ MVVM/ViewModel/PaceConversionViewModel.cs
@@ -5,9 +5,72 @@ using System.Collections.ObjectModel;
 
 namespace PaceCalculator.MVVM.ViewModel
 {
+    public enum PaceConversionMode { MI_TO_KM = 1, KM_TO_MI = 2 }
+
     public class PaceConversionViewModel : ObservableObject
     {
+        private string mileMinutes;
+        public string MileMinutes
+        {
+            get { return mileMinutes; }
+            set 
+            { 
+                mileMinutes = value;
+                OnPropertyChanged(nameof(MileMinutes));
+            }
+        }
+
+        private string mileSeconds;
+        public string MileSeconds
+        {
+            get { return mileSeconds; }
+            set
+            {
+                mileSeconds = value;
+                OnPropertyChanged(nameof(MileSeconds));
+            }
+        }
+
+        private string kmMinutes;
+        public string KmMinutes
+        {
+            get { return kmMinutes; }
+            set
+            {
+                kmMinutes = value;
+                OnPropertyChanged(nameof(KmMinutes));
+            }
+        }
+
+        private string kmSeconds;
+        public string KmSeconds
+        {
+            get { return kmSeconds; }
+            set
+            {
+                kmSeconds = value;
+                OnPropertyChanged(nameof(KmSeconds));
+            }
+        }
+
+        private PaceConversionMode conversionMode;
+        public PaceConversionMode ConversionMode
+        {
+            get { return conversionMode; }
+            set
+            {
+                conversionMode = value; 
+                OnPropertyChanged(nameof(ConversionMode)); 
+            }
+        }
+
         public PaceConversionViewModel()
-        { }
+        {
+            mileMinutes = "";
+            mileSeconds = "";
+            kmMinutes = "";
+            kmSeconds = "";
+            conversionMode = PaceConversionMode.MI_TO_KM;
+        }
     }
 }
